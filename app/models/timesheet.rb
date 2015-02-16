ff8985c2ebf8d70fcd5a032c3b1df3bab0599af5
chiperific <chiperific@gmail.com>
2015-02-15 23:23:28 -0500

    remove weeknum_to_date

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -6,16 +6,6 @@ class Timesheet < ActiveRecord::Base
   accepts_nested_attributes_for :timesheet_hours, :timesheet_categories
 
   validates :start_date, :end_date, presence: true
-  #validates :week_num, numericality: { greater_than: 0, less_than_or_equal_to: 52 }
-  #validates :year, numericality: { greater_than_or_equal_to: 1900, less_than_or_equal_to: 2999 }
-
-  def week_num_to_date
-    Date.commercial(self.year, self.week_num, 1).strftime("%m/%d/%Y")
-  end
-
-  def week_num_to_date_obj
-    Date.commercial(self.year, self.week_num, 1).strftime("%Y/%m/%d")
-  end
 
   def status
     if self.hours_approved.present?
